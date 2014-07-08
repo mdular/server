bf9884f5cfdb350e576bd194b7584883c2fe0f6a
mdular <git@the-enemy.org>
2014-07-09 00:42:00 +0200

    add composer to php component

--- puppet/components/component_mdular_php/manifests/init.pp
+++ puppet/components/component_mdular_php/manifests/init.pp
@@ -1,17 +1,27 @@
 # initialize the php module
 class component_mdular_php {
 
+  # ensure dependencies for installation without apache
   package { ["php5-cli", "php5-cgi", "php-pear"]:
     ensure => installed,
   } ->
 
+  # fix example42 php module trying to write apache2 php config
   file { "/etc/php5/apache2": 
     ensure => directory,
   } ->
 
+  # initialize example42 php module
   class { 'php':
-    service => 'nginx'
+    service => 'nginx',
+    #version => '5.4.4', # TODO: doesn't work?
   }
 
+  # additional php modules
   php::module { ["fpm", "mysql"]: }
+
+  # install composer
+  class { 'composer':
+    auto_update => true
+  }
 }
