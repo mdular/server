c098dd36a3022505076d068bbc8ae621c086941b
Markus J Doetsch <git@the-enemy.org>
2015-07-08 13:43:49 +0200

    disable nodeapp role for now

--- puppet/manifests/03-role_nodeapp.pp
+++ puppet/manifests/03-role_nodeapp.pp
@@ -1,8 +1,8 @@
 class role_nodeapp inherits role_webserver {
-  # TODO: role_webserver because of nginx.. 
+  # TODO: role_webserver because of nginx..
   # consider creating role_proxy for firewall and nginx, since
   # mysql and php are not required for role_nodeapp
 
   # nodejs component
-  class { 'component_mdular_nodejs': }
+  #class { 'component_mdular_nodejs': }
 }
