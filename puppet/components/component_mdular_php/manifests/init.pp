c42b9fbc984c6f7612560d3cf9a64956581e289c
Markus J Doetsch <git@the-enemy.org>
2015-08-07 02:46:09 +0200

    add php-curl

--- puppet/components/component_mdular_php/manifests/init.pp
+++ puppet/components/component_mdular_php/manifests/init.pp
@@ -24,7 +24,7 @@ class component_mdular_php {
   }
 
   # additional php modules
-  php::module { ["mysql", "gd"]: }
+  php::module { ["mysql", "gd", "curl"]: }
 
   php::module { ["apc"]:
     module_prefix => 'php-',
