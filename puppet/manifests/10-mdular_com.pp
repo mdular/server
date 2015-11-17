c80bb263f4fc56129c104bbb7fb2ed3ead874ab8
Markus J Doetsch <git@the-enemy.org>
2015-11-17 01:14:56 +0100

    upgrade nginx config

--- puppet/manifests/10-mdular_com.pp
+++ puppet/manifests/10-mdular_com.pp
@@ -6,17 +6,27 @@ class mdular_com (
     $mysql = {}, #hiera("mdular_com::mysql")
     $htpasswd = {} #hiera("mdular_com::htpasswd")
   ) inherits role_webserver {
-  
+
   # users, groups
   create_resources(user, $users)
   create_resources(group, $groups)
 
   # create host
-  web::nginx_host { 'mdular.com': 
+  # todo: create as resource from hiera
+  web::nginx_host { 'mdular.com':
     www_root => "/var/www/mdular.com/public",
-    listen_options => "default_server",
+    # listen_options => "default_server",
+    server_name => ['mdular.com', 'mdular.dev']
     #htpasswd => true,
     #backend_port => 9001,
+    location_cfg_append => {
+      fastcgi_connect_timeout => '3m',
+      fastcgi_read_timeout    => '3m',
+      fastcgi_send_timeout    => '3m',
+      fastcgi_param => {
+          'APP_ENV' => 'live'
+      }
+    }
   }
 
   # mysql db, user, grant
